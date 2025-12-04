90f76d2c2a7b52bc57d7318161f05931509961b8
Chase_S <58236767+chase-sabanski@users.noreply.github.com>
2025-12-04 15:15:05 -0500

    Update python_script_efficiency_calculator.py
    Removed math module.
    Edited the first text input.
    Cleaned up how the time is calculated; put in variables instead of calculating withing the print() lines.
    Edits made to improve readability.

--- python_script_efficiency_calculator.py
+++ python_script_efficiency_calculator.py
@@ -1,4 +1,3 @@
-import math
 import pyinputplus as pyip
 
 def main():
@@ -11,7 +10,7 @@ def main():
         # input validation loop that only accepts non-negative integers
         while True:
             try:
-                ticket_count = input("How many tickets have been created via CSV: ")
+                ticket_count = input("Please enter the amount of tickets created using the python upload script: ")
                 int_input = int(ticket_count)
 
                 if int_input < 0:
@@ -26,21 +25,30 @@ def main():
         # ...multipling the input by 10 min (generous estimate of how long it takes to create a ticket) then,
         # ...dividing the input by 60 to get total hours then,
         # ...dividing the input by 8 to get total workdays then,
-        # ...rounding up
-        manual_time_workdays = math.ceil((((int_input * 10) / 60) / 8))
+        # ...rounding up at each calculation
+        minutes = round(int_input * 10)
+        hours = round(minutes / 60)
+        days = round(hours / 8)
+        work_weeks = round(days / 5)
+        years = round(days / 260) # this represents a work year, not a whole calendar year
+        team_effort = round(days / 5)
 
         print(f"""
         Time saved:
-        Minute(s): {(int_input * 10)}
-        Hour(s): {(int_input * 10) / 60}
-        Day(s): {manual_time_workdays}
-        Work week(s): {(manual_time_workdays / 5)}
-        Year(s): {((((int_input * 10) / 60) / 8) / 260)}
+        Minute(s): {minutes}
+        Hour(s): {hours}
+        Day(s): {days}
+        Work week(s): {work_weeks}
+        Year(s): {years}
         """)
 
+        # stylistic choice so that each new line starts with a number
         print(f"""
-        It would take a team of 5 experienced analysts approximately {round(((manual_time_workdays / 5) / 5))} weeks 
-        to create {int_input} tickets manually if that's all they did everyday for 8 hours a day.
+        It would take a team of
+        5 experienced analysts approximately
+        {team_effort} days to create
+        {ticket_count} tickets manually if that's all they did everyday for
+        8 hours a day.
         """)
 
         # loop that allows the user to keep running the program until they type "n" in the terminal
