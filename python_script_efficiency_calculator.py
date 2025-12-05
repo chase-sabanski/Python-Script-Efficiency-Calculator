28545534268d8b1c49a840c1357ba934e53593b1
Chase_S <58236767+chase-sabanski@users.noreply.github.com>
2025-12-05 09:56:42 -0500

    Update python_script_efficiency_calculator.py
    Included input validation that would prevent obscenely large numbers from being entered and therefore crashing the script.
    
    Included "analysts" variable that makes it easier to adjust how much labor is saved based on the size of the current team.
    
    Output changes for clarity and style.

--- python_script_efficiency_calculator.py
+++ python_script_efficiency_calculator.py
@@ -14,7 +14,10 @@ def main():
                 int_input = int(ticket_count)
 
                 if int_input < 0:
-                    print("Please enter a non-negative integer.")
+                    print("Please enter a non-negative integer.\n")
+                    continue
+                elif int_input > 1000000000:
+                    print("Nice try, but it's extremely unlikely that many tickets have been created. Please enter an integer smaller than 1,000,000,000.\n")
                     continue
                 break
 
@@ -25,38 +28,41 @@ def main():
         # ...multipling the input by 10 min (generous estimate of how long it takes to create a ticket) then,
         # ...dividing the input by 60 to get total hours then,
         # ...dividing the input by 8 to get total workdays then,
-        # ...rounding up at each calculation
+        # ...rounding up at each calculation to avoid large decimals from being printed
         minutes = round(int_input * 10)
         hours = round(minutes / 60)
         days = round(hours / 8)
         work_weeks = round(days / 5)
         years = round(days / 260) # this represents a work year, not a whole calendar year
-        team_effort = round(days / 5)
+        analysts = 5 # edit this variable depending on the current size of the team
+        team_effort = round(days / analysts)
 
         print(f"""
-        Time saved:
-        Minute(s): {minutes}
-        Hour(s): {hours}
-        Day(s): {days}
-        Work week(s): {work_weeks}
-        Year(s): {years}
+        Amount of labor saved in...
+         > Minute(s): {minutes:,d}
+         > Hour(s): {hours:,d}
+         > Day(s): {days:,d}
+         > Work week(s): {work_weeks:,d}
+         > Year(s): {years:,d}
         """)
 
         # stylistic choice so that each new line starts with a number
         print(f"""
         It would take a team of
-        5 experienced analysts approximately
-        {team_effort} days to create
-        {ticket_count} tickets manually if that's all they did everyday for
+        {analysts} experienced analysts approximately
+        {team_effort:,d} days to create
+        {int_input:,d} tickets manually if that's all they did everyday for
         8 hours a day.
         """)
 
-        # loop that allows the user to keep running the program until they type "n" in the terminal
-        prompt = "Run again? (Y/N):\n"
+        # prompt that allows the user to break the while loop if they type "n" in the terminal
+        prompt = "Run again? (Y/N): "
         response = pyip.inputYesNo(prompt, yesVal="y", noVal="n")
         if response == "n":
-            print("Exiting...")
+            print("\nExiting...\n",)
             break
+        else:
+            print()
 
 if __name__=="__main__":
     main()
